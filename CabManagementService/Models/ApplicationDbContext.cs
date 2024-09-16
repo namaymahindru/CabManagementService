19be28892c793110a7ba713e0c7b15ce97fb8b43
Namay Mahindru <155489911+namaymahindru@users.noreply.github.com>
2024-09-16 23:58:44 +0530

    Update ApplicationDbContext.cs

--- CabManagementService/Models/ApplicationDbContext.cs
+++ CabManagementService/Models/ApplicationDbContext.cs
@@ -7,27 +7,17 @@
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
-        public DbSet<CarBookModel> carBookModels { get; set; }
-        public DbSet<UserRegistrationModel> userRegistrationModels { get; set; }
-        public DbSet<ContactUsModel> contactUsModels { get; set; }
+            public DbSet<CarBookModel> carBookModels { get; set; }
+            public DbSet<UserRegistrationModel> userRegistrationModels { get; set; }
+            public DbSet<ContactUsModel> contactUsModels { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-
-
-
             builder.Entity<CarBookModel>().ToTable("CarRegistration");
-             builder.Entity<UserRegistrationModel>().ToTable("UserRegistration");
+            builder.Entity<UserRegistrationModel>().ToTable("UserRegistration");
             builder.Entity<ContactUsModel>().ToTable("ContactAdmin");
 
-
-
         }
-
-
-
-
-
 
     }
 }
